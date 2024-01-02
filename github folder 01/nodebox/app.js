ed6de99b8f80c127fa27c23e8d1020220c9e9fd4
sundaramtarone-stuff <153430347+sundaramtarone-stuff@users.noreply.github.com>
2024-01-02 11:39:28 +0530

    Add files via upload

---
+++ github folder 01/nodebox/app.js
@@ -0,0 +1,95 @@
+const carousel = document.querySelector(".carousel");
+let sliders = [];
+
+let slideIndex = 0;
+
+const createSlide = () => {
+  if (slideIndex >= movies.length) {
+    slideIndex = 0;
+  }
+
+  // <div class="slider">
+  //             <div class="slide-content">
+  //                 <h1 class="movie-title">Loki</h1>
+  //                 <p class="movie-des">Loki, the God of Mischief, steps out of his brother's shadow to embark on an adventure that takes place after the events of Avengers: Endgame.</p>
+  //             </div>
+  //             <img src="./images/slider 1.png" alt="">
+  //         </div>
+
+  // creating DOM element
+  let slide = document.createElement("div");
+  let imgElement = document.createElement("img");
+  let content = document.createElement("div");
+  let h1 = document.createElement("h1");
+  let p = document.createElement("p");
+
+  //attaching all element
+  imgElement.appendChild(document.createTextNode(""));
+  h1.appendChild(document.createTextNode(movies[slideIndex].name));
+  p.appendChild(document.createTextNode(movies[slideIndex].des));
+  content.appendChild(h1);
+  content.appendChild(p);
+  slide.appendChild(content);
+  slide.appendChild(imgElement);
+  carousel.appendChild(slide);
+
+  // setting up image
+  imgElement.src = movies[slideIndex].image;
+  slideIndex++;
+
+  // setting element classname
+  slide.className = "slider";
+  content.className = "slide-content";
+  h1.className = "movie-title";
+  p.className = "movie-des";
+
+  sliders.push(slide);
+
+  //adding sliding effect
+  if (sliders.length) {
+    sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+      30 * (sliders.length - 2)
+    }px)`;
+  }
+};
+
+for (let i = 0; i < 3; i++) {
+  createSlide();
+}
+
+setInterval(() => {
+  createSlide();
+}, 3000);
+
+// video cards
+const videoCards = [...document.querySelectorAll(".video-card")];
+
+videoCards.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    let video = item.children[1];
+    video.play();
+  });
+  item.addEventListener("mouseleave", () => {
+    let video = item.children[1];
+    video.pause();
+  });
+});
+
+// card sliders
+
+let cardContainers = [...document.querySelectorAll(".card-container")];
+let preBtns = [...document.querySelectorAll(".pre-btn")];
+let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+
+cardContainers.forEach((item, i) => {
+  let containerDimension = item.getBoundingClientRect();
+  let containerWidth = containerDimension.width;
+
+  nxtBtns[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth - 100;
+  });
+
+  preBtns[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth + 100;
+  });
+});
---
+++ github folder 01/nodebox/index.js
@@ -0,0 +1,25 @@
+{
+  name:'loki',
+  des:'The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.”',
+  image:'images/slider 1.PNG'
+},
+{
+  name:'Black Widow',
+  des:'Natasha Romanoff confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises.',
+  image:'images/slider 2.PNG'
+},
+{
+  name:'Cruella',
+  des:'The film is about the rebellious early days of cinema most notorious and fashionable villains, the legendary Cruella de Vil.',
+  image:'images/slider 3.PNG'
+},
+{
+  name:'What If...?',
+  des:'NEW EPISODE EVERY WEDNESDAY AT 12:30 PM. What If…? flips the script on the MCU, reimagining famous events from the films in unexpected ways.',
+  image:'images/slider 4.PNG'
+},
+{
+  name:'luca',
+  des:'“Luca” tells the story of two male sea monsters who have a friendship so intimate that many critics interpreted it as a gay allegory',
+  image:'images/slider 5.PNG'
+}
